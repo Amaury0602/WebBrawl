8ae08e3be51974d5308c9edcb375d535be464708
amaury ropert <amauryropert@gmail.com>
2019-09-27 13:06:06 +0200

    bataille aléatoire, le gagnant par défaut est le premier fighter choisi

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,5 +1,7 @@
 import "bootstrap";
 
 import { selectFighters } from '../plugins/select_fighters.js.erb';
+import { battleAttack } from '../plugins/battle_attack.js.erb';
 
 selectFighters();
+battleAttack();
