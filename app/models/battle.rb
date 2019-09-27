5adf4cc7627819ad170813ff19143d3af19dc1b5
amaury ropert <amauryropert@gmail.com>
2019-09-27 18:58:41 +0200

    encore un peu de css sur index_fighter + battleground

--- app/models/battle.rb
+++ app/models/battle.rb
@@ -2,4 +2,8 @@ class Battle < ApplicationRecord
   belongs_to :your_fighter, class_name: "Fighter"
   belongs_to :enemy_fighter, class_name: "Fighter"
   belongs_to :winner, class_name: "Fighter"
+
+  def loser
+    self.winner == self.your_fighter ? self.enemy_fighter : self.your_fighter
+  end
 end
