d1d6a9cb47e98259aadcc19220f3d08c7203483a
amaury ropert <amauryropert@gmail.com>
2019-09-29 23:01:29 +0200

    battle is better

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,9 +11,9 @@ puts "everything destroyed"
 
 hand = Equipment.create(mode: "offense", name: "Bare Knuckles", value: 0, level: 0);
 small_shield = Equipment.create(mode: "defense", name: "Small shield", value: 2, level: 1);
-wood_sword = Equipment.create(mode: "offense", name: "Wooden sword", value: 3, level: 3)
-great_shield = Equipment.create(mode: "defense", name: "Great shield", value: 4, level: 5)
-long_sword = Equipment.create(mode: "offense", name: "Long sword",  value: 4, level: 6)
+wood_sword = Equipment.create(mode: "offense", name: "Wooden sword", value: 2, level: 3)
+great_shield = Equipment.create(mode: "defense", name: "Great shield", value: 3, level: 5)
+long_sword = Equipment.create(mode: "offense", name: "Long sword",  value: 3, level: 6)
 
 
 5.times do
