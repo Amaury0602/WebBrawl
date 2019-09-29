c599acf93542aad3a5240fb5ecb6c91833912819
amaury ropert <amauryropert@gmail.com>
2019-09-29 18:25:07 +0200

    new css; new design

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -1,4 +1,8 @@
 class Equipment < ApplicationRecord
   has_many :left_hand, class_name: "Fighter", foreign_key: "left_hand_id"
   has_many :right_hand, class_name: "Fighter", foreign_key: "right_hand_id"
+
+
+  def self.can_handle(fighter_level)
+  end
 end
