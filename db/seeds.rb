297cd37f7d4d14e656492a1b73676ac7376c88ae
amaury ropert <amauryropert@gmail.com>
2019-09-29 12:20:03 +0200

    reworked seed + show

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,13 @@ puts "everything destroyed"
 hand = Equipment.create(mode: "offense", name: "Bare Knuckles", value: 0, level: 0);
 small_shield = Equipment.create(mode: "defense", name: "Small shield", value: 2, level: 1);
 wood_sword = Equipment.create(mode: "offense", name: "Wooden sword", value: 3, level: 3)
+great_shield = Equipment.create(mode: "defense", name: "Great shield", value: 4, level: 5)
+long_sword = Equipment.create(mode: "offense", name: "Long sword",  value: 4, level: 6)
 
 
 5.times do
   Fighter.create(
-    name: Faker::Superhero.name,
+    name: Faker::Superhero.name.delete(" "),
     health: 10,
     attack: 1,
     left_hand: Equipment.first,
